e628bf39cef81ab331d7c3aa66868a0a7f11e245
John Costanzo <jrock2004@gmail.com>
2021-08-05 23:02:37 -0400

    feat: setting up netlify

---
+++ functions/anime/anime.ts
@@ -0,0 +1,15 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+export { handler }
