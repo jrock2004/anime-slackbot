e4dd42694953f26377cedc0f810a28049da3dba6
John Costanzo <jrock2004@gmail.com>
2024-11-25 22:25:15 -0500

    test: updating coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: [
+        '**/node_modules/**',
+        '**/.netlify/**',
+        '**/*.d.ts',
+        'commitlint.config.js',
+        'eslint.config.mjs',
+        'vitest.config.ts',
+      ],
       provider: 'v8', // or 'v8' or 'instanbul'
       reporter: ['cobertura', 'text', 'json', 'html'],
     },
